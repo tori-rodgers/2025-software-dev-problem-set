fb50cd2eb4c68ef92fe28968ecad785ab117dc88
Tori Rodgers <torirodgers22@gmail.com>
2025-08-07 17:54:34 -0500

    found better solution and added notes for understanding

--- solutions/01-Contains-Duplicate.js
+++ solutions/01-Contains-Duplicate.js
@@ -3,8 +3,10 @@
 // Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
  
-const nums = [1,2,3,4,5,6,6];
+// This solution is the same time space complexity (O(n)) but far more steps.
+// Both use Sets, which are similar to Hash Sets in Java & Python.
 
+/*
 class Solution {
     hasDuplicate(nums) {
         const seen = new Set();
@@ -20,3 +22,31 @@ class Solution {
 
 const solution = new Solution();
 console.log(solution.hasDuplicate(nums));
+*/
+
+
+class Solution {
+    hasDuplicate(nums) {
+        return new Set(nums).size < nums.length;
+    }
+}
+
+const solution = new Solution();    // Create a new instance of Solution
+
+const nums = [1,2,3,4,5,6,6];
+console.log(solution.hasDuplicate(nums));
+
+// or 
+
+console.log(solution.hasDuplicate([1, 2, 3, 3, 4]));
+console.log(solution.hasDuplicate([1, 2, 3, 4]));
+
+
+/* Using the class Solution is standard for Leetcode, also allows encapsulation & reusability. 
+Adds utitility for OOP (ex. inheritance)
+Set is a built-in JS object that stores *unique* values. 
+When the nums array is passed into new Set, it removes the duplicates. 
+The return evaluates the length of the new Set and the original array. 
+Returns true if the length of the new Set is less than the length of the original array / false if they are the same. 
+*/
+
